9fb3fe212065ceb7bdb6fb77ab0f7fee8093aacf
tommy.huang <tommy.huang@ptc-nec.com.tw>
2020-01-06 10:56:13 +0800

    新增使用者紀錄

--- linebot_blackcat/blackcat.js
+++ linebot_blackcat/blackcat.js
@@ -16,6 +16,10 @@ const parser = bodyParser.json({
   }
 });
 
+/** 使用者 */
+var userList = [];
+
+
 app.post('/linewebhook', parser, function (req, res) {
   if (!bot.verify(req.rawBody, req.get('X-Line-Signature'))) {
     return res.sendStatus(400);
@@ -25,31 +29,138 @@ app.post('/linewebhook', parser, function (req, res) {
 });
 
 // 加入聊天室時跟對方打招呼
-bot.on('join', function (event) {
+bot.on('follow', function (event) {
   event.source.profile().then(function (profile) {
-    event.push(event.source.userId, event.message.text + profile.displayName).then(function (data) {
+    if(event.source.type == 'user'){
+      /*
+      if(!userList.user.id.find(profile.userId)){
+
+      }
+      */
+      user = {
+        id:event.source.userId,
+        displayName:profile.displayName
+      }
+      userList = userList.concat([{
+        user:user
+      }]);
+    }
+
+
+    bot.push(
+      event.source.userId, 
+      profile.userId +　profile.displayName
+    ).then(function (data) {
       console.log('Success', data);
     }).catch(function (error) {
       console.log('Error', error);
     });
+
+    console.dir("///////////////debug push/////////");
+    console.dir("userList");
+    console.dir(userList);
+    console.dir(Object.keys(userList));
+    var isExist = userList.filter(function(item, index, array){
+      console.log("item");
+      console.log(item);
+      console.log("array");
+      console.log(array);
+      return item.user.displayName ==  profile.displayName;
+    });
+    
+    /*
+    .then( ()=>{
+        if(isExist.Length > 0){
+          console.log("isExist");
+          console.log(isExist);
+        }
+      }
+    )
+    */
+
+    console.dir("profile");
+    console.dir(profile);
+    console.dir("////////////////////////");
+
+
   });
 });
 
 bot.on('message', function (event) {
   event.source.profile().then(function (profile) {
+    //console.log(userList.user.id.find(profile.userId));
+    if(event.source.type == 'user'){
+
+      /*
+      for(i = 0,i < userList.Length,i++){
+
+      }
+      */
+          user = {
+          id:event.source.userId,
+          displayName:profile.displayName
+          }
+          userList = userList.concat([{
+            user:user
+          }])
+     // }
+    }
+
     if(event.message.text == '資料'){
-      event.reply(event.source.userId + " \n " + profile.displayName).then(function (data) {
+        event.reply(event.source.userId + " \n " + profile.displayName).then(function (data) {
         console.log('Success', data);
       }).catch(function (error) {
         console.log('Error', error);
       });
     }
+
+   
+    console.dir("///////////////debug reply/////////");
+    console.dir("event:");
     console.dir(event);
+    console.dir("event.message:");
     console.dir(event.message);
+    console.dir("event.message.text:");
     console.dir(event.message.text);
     console.log("\n");
+    console.dir("profile");
+    console.dir(profile);
+    console.dir("userList");
+    console.dir(userList);
+    console.dir("////////////////////////");
   });
 });
+
+/*
+if(event.message.text.indexOf("宏") >= 0) {
+  event.reply(event.message.text + "好帥").then(function (data) {
+    console.log('Success', data);
+  }).catch(function (error) {
+    console.log('Error', error);
+  });
+}
+if(event.message.text.indexOf("女友") >= 0) {
+  event.reply("我全部建議分手").then(function (data) {
+    console.log('Success', data);
+  }).catch(function (error) {
+    console.log('Error', error);
+  });
+}
+if(event.message.text.indexOf("菸") >= 0) {
+  event.reply("戒掉").then(function (data) {
+    console.log('Success', data);
+  }).catch(function (error) {
+    console.log('Error', error);
+  });
+}
+if(event.message.text == "一根菸抽不夠") {
+  event.reply("那就抽兩根").then(function (data) {
+    console.log('Success', data);
+  }).catch(function (error) {
+    console.log('Error', error);
+  });
+}
+*/
 
 /*
 bot.on('message', function (event) {
@@ -80,6 +191,6 @@ bot.on('message', function (event) {
 });
 */
 
-app.listen(process.env.PORT || 3200, function () {
+app.listen(process.env.PORT || 3201, function () {
   console.log('LineBot is running.');
 });
